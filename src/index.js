9c58565bd8a92acf58642a96d0b98b74d925fa3e
Yurii <Rapacta@gmail.com>
2022-06-02 00:54:11 +0200

    to cheked

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import fetchCountry from './js/fetchCountries'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.querySelector('#search-box');
+const listRef = document.querySelector('.country-list');
+const containerRef = document.querySelector('.country-info');
+
+inputRef.addEventListener('input', debounce(onFetchCountry, DEBOUNCE_DELAY));
+
+function onFetchCountry(event) {
+  event.preventDefault();
+
+  const valueInput = event.target.value.trim();
+  listRef.innerHTML = '';
+  containerRef.innerHTML = '';
+
+  fetchCountry(valueInput)
+    .then(response => {
+      console.log(response);
+      if (response.length > 10) {
+        Notify.info("Too many matches found. Please enter a more specific name.")
+      } else if (response.length >= 2 && response.length <= 10) {
+        renderCountryList(response);
+      } else if (response.length === 1) {
+        renderCountryInfo(response);
+      }
+    })
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
+};
+
+function renderCountryList(country) {
+  const countriesList = country.map((count) => {
+    return `<li class="country-item">
+	            <h2 class="country-name">
+		            <img src=" ${count.flag}" alt="flags ${count.name}" width="35px">
+		            ${count.name}
+	             </h2>
+            </li>`
+  }).join("")
+  listRef.innerHTML = countriesList;
+}
+
+function renderCountryInfo(country) {
+
+  const countriesList = country.map((count) => {
+    return `<li class="country-item">
+	            <h2 class="country-name">
+		            <img src=" ${count.flag}" alt="flags ${count.name}" width="35px">
+		            ${count.name}
+	             </h2>
+            </li>`
+  }).join("")
+  listRef.innerHTML = countriesList;
+
+  const countriesInfo = country.flatMap((count) => {
+    return `<ul class="country-info__list">
+	<li class="country-info__item" > <b>Capital:</b>  ${count.capital} </li>
+	<li  class="country-info__item"> <b>Population:</b>  ${count.population}</li>
+	<li class="country-info__item" > <b>Languages:</b>  ${count.languages.name} </li>
+</ul>`
+  });
+
+  listRef.innerHTML = countriesList;
+  containerRef.innerHTML = countriesInfo;
+}
