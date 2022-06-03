9dd678b1807338d93056d8f0fa5b7629762b09b2
Yurii <Rapacta@gmail.com>
2022-06-03 18:18:03 +0200

    test function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import fetchCountry from './js/fetchCountries'
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import renderCountryInfo from './js/render_Country_info';
-import renderCountryList from './js/render_Country_List';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import renderCountryInfo from './js/render_Country_info';
+// import renderCountryList from './js/render_Country_List';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -33,3 +34,38 @@ function onFetchCountry(event) {
     })
     .catch(error => Notify.failure('Oops, there is no country with that name'));
 };
+
+function renderCountryList(country) {
+  const countriesList = country.map((count) => {
+    return `<li class="country-item">
+	            <h2 class="country-name">
+		            <img src=" ${count.flag.svg}" alt="flags ${count.name}" width="35px">
+		            ${count.name}
+	             </h2>
+            </li>`
+  }).join("")
+  listRef.innerHTML = countriesList;
+}
+
+function renderCountryInfo(country) {
+
+  const countriesList = country.map((count) => {
+    return `<li class="country-item">
+	            <h2 class="country-name">
+		            <img src=" ${count.flag}" alt="flags ${count.name}" width="35px">
+		            ${count.name}
+	             </h2>
+            </li>`
+  }).join("")
+
+
+  const countriesInfo = country.map((count) => {
+    return `<ul class="country-info__list">
+              <li class="country-info__item" > <b>Capital:</b>  ${count.capital} </li>
+              <li  class="country-info__item"> <b>Population:</b>  ${count.population} </li>
+              <li class="country-info__item" > <b>Languages:</b>  ${count.languages} </li>
+            </ul>`
+  });
+  containerRef.innerHTML = countriesInfo;
+  listRef.innerHTML = countriesList;
+}
--- src/js/render_Country_info.js
+++ src/js/render_Country_info.js
@@ -13,11 +13,9 @@ export default function renderCountryInfo(country) {
     const countriesInfo = country.map((count) => {
         return `<ul class="country-info__list">
               <li class="country-info__item" > <b>Capital:</b>  ${count.capital} </li>
-              <li  class="country-info__item"> <b>Population:</b>  ${count.population}</li>
+              <li  class="country-info__item"> <b>Population:</b>  ${count.population} </li>
               <li class="country-info__item" > <b>Languages:</b>  ${count.languages} </li>
             </ul>`
     });
-
-    listRef.innerHTML = countriesList;
     containerRef.innerHTML = countriesInfo;
 }
